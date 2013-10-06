51fd5db8d15152fd27b38f02aeb7a53c034dc54a
Yasyf Mohamedali <yasyf@yasyf.com>
2013-10-05 21:17:14 -0400

    initial working API for creating notifications

---
+++ pebble_api/PebbleAPI.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+from functions import *
+
+app = Flask(__name__)
+app.secret_key = '\x0f?\xad6\x19\xfe\x1b\x1buy1" \xb14\x94\xeb2L\x85\x9e\x04RR'
+
+	
+@app.route('/')
+def index():
+	if 'userid' in session:
+		return render_template('index.html')
+	else:
+		return redirect(url_for('login', error="You must log in to do that."))
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+	if request.method == 'POST':
+		username = request.form.get('username','')
+		password = request.form.get('password','')
+		if check_credentials(username,password):
+			session["userid"] = get_userid(username)
+			return redirect(url_for('index'))
+		elif validate_credentials(username,password):
+			session["userid"] = create_user(username,password)
+			return redirect(url_for('index'))
+		else:
+			return render_template('login.html',error="Your credentials were invalid.")
+	else:
+		if 'userid' in session:
+			return redirect(url_for('login'))
+		else:
+			return render_template('login.html',error=request.args.get('error', ''))
+
+@app.route('/api/notification/create/<userid>/<source>/<text>', methods=['POST', 'GET'])
+def new_notification(userid, source, text):
+	if check_userid(userid):
+		if len(source) > 0 and len(text) > 0:
+			return create_notification(userid, source, text)
+		else:
+			error = "missing required parameter"
+	else:
+		error = "invalid userid"
+	return show_error(error)
+
+@app.route('/api/notification/get/<userid>/<notificationid>', methods=['POST', 'GET'])
+def retreive_notification(userid, notificationid):
+	if check_userid(userid) and check_notificationid(notificationid) and compare_ids(userid, notificationid):
+		return get_notification(notificationid, userid)
+	else:
+		return show_error("invalid notificationid for this userid")
+
+	
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ pebble_api/functions.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import hashlib, datetime, json, calendar
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from bson import json_util
+
+client = MongoClient(open('db.txt',"r").read().strip())
+db = client.hackmit
+users = db.users
+notifications = db.notifications
+
+def isset_credentials(username, password):
+	return (len(username) > 0) and (len(password) > 0)
+
+def validate_credentials(username, password):
+	return isset_credentials(username, password) and (users.find({"username": username}).count() == 0)
+
+def check_credentials(username, password):
+	return isset_credentials(username, password) and (users.find({"username": username}).count() != 0) and (sha1(password) == users.find({"username": username})[0]["password"])
+
+def get_userid(username):
+	return str(users.find({"username": username})[0]["_id"])
+
+def check_userid(userid):
+	return (users.find({"_id": ObjectId(userid)}).count() == 1)
+
+def check_notificationid(notificationid):
+	return (notifications.find({"_id": ObjectId(notificationid)}).count() == 1)
+	
+def sha1(text):
+	m = hashlib.sha1()
+	m.update(text)
+	return m.hexdigest()
+
+def json_encode(k,v):
+	return json.dumps({k: v})
+
+	
+def create_user(username, password):
+	return str(users.insert({"username": username, "password": sha1(password)}))
+	
+def create_notification(userid, source, text):
+	return json_encode("notificationid", str(notifications.insert({"userid": userid, "time": datetime.datetime.utcnow(), "source": source, "text": text, "delivered": "false"})))
+
+def get_notification(notificationid, userid):
+	obj = notifications.find({"_id": ObjectId(notificationid), "userid": userid})[0]
+	del obj["_id"]
+	obj["time"] = calendar.timegm(obj["time"].timetuple())
+	return json.dumps(obj)
+
+def compare_ids(userid, notificationid):
+	return (userid == notifications.find({"_id": ObjectId(notificationid)})[0]["userid"])
+	
+def show_error(text):
+	return json_encode("error",text)
+		
+def mark_notification_delivered(notificationid, delivered):
+	if delivered == True:
+		status = "true"
+	else:
+		status = "false"
+	return str(notifications.update({"_id": ObjectId(notificationid)}, {"$set": {"delivered": status}}))
